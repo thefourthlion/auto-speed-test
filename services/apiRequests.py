eb6c15f82b1bf797f691ca6ff54c9f7b1f9023c3
TheFourthLion <the00110100lion@gmail.com>
2023-12-22 06:02:29 -0800

    add api tests

--- services/apiRequests.py
+++ services/apiRequests.py
@@ -1,10 +1,21 @@
 import requests
+from datetime import datetime
 
-api_url = "http://localhost:3002/"
+# Getting the current date and time
+current_date_and_time = datetime.now()
 
-def postData(api_url, Ip, name, download, upload, ping):
-    data = {"IP": Ip, "name": name, "download": download, "upload": upload, "ping": ping}
+# Formatting the date and time in the specified format: "time-mm/dd/yr"
+formatted_date_and_time = current_date_and_time.strftime("time-%m/%d/%Y")
+
+
+api_url = "http://localhost:3002/api/speeds/read"
+
+def postData(Ip, name, download, upload, ping, timestamp):
+    api_url = "http://localhost:3002/api/speeds/create"
+    data = {"Ip": Ip, "name": name, "download": download, "upload": upload, "ping": ping, "timestamp":timestamp}
     response = requests.post(api_url, json=data)
+
+# postData("192.168.1.1", "everett_pc", "850", "20", "22", formatted_date_and_time)
 
 def getData(api_url):
     response = requests.get(api_url)
@@ -13,4 +24,4 @@ def getData(api_url):
     return data
 
 
-print(getData(api_url))
+# print(getData(api_url))
