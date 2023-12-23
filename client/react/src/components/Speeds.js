4851aabc1fd679480c9f46d5da17245d6d3a304e
TheFourthLion <the00110100lion@gmail.com>
2023-12-23 05:03:59 -0800

    add react client

---
+++ client/react/src/components/Speeds.js
@@ -0,0 +1,11 @@
+import React from "react"; 
+ const Speeds = () => { 
+ return ( 
+ <div className="Speeds"> 
+ <div className="container"> 
+ <h1 className="content-header">Speeds</h1> 
+ </div> 
+ </div> 
+ ); 
+ }; 
+ export default Speeds;
