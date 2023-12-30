637111b8977bdcc9eaf1a7f3bc4ef4367f1a2b37
TheFourthLion <the00110100lion@gmail.com>
2023-12-30 07:33:05 -0800

    make speed test on timer and add upload and download as list

--- services/main.py
+++ services/main.py
@@ -6,51 +6,64 @@ import requests
 import apiRequests as req
 from datetime import datetime
 import pytz
+import time
 
-#---------- get date and time -----------------------
-pst = pytz.timezone('US/Pacific')
+def speedTest():
+    #---------- get date and time -----------------------
+    pst = pytz.timezone('US/Pacific')
 
-utc_now = datetime.now(pytz.utc)
-pst_now = utc_now.astimezone(pst)
-date_and_time = pst_now.strftime('%m-%d-%Y %H:%M')
+    utc_now = datetime.now(pytz.utc)
+    pst_now = utc_now.astimezone(pst)
+    date_and_time = pst_now.strftime('%m-%d-%Y %H:%M')
 
-print("Testing Speed...⏳")
+    # ---------- ip addr --------------------------------
+    ipAddr = socket.gethostbyname(socket.gethostname())
+    publicIp = requests.get('https://checkip.amazonaws.com').text.strip()
+    print(f"IP Address: {ipAddr}")
+    print(f"Public IP: {publicIp}")
+    
+    print("Running Speed test...⏲️")
+    # ---------- speed test --------------------------------
+    speedTest = speedtest.Speedtest()
 
-# ---------- ip addr --------------------------------
-ipAddr = socket.gethostbyname(socket.gethostname())
-publicIp = requests.get('https://checkip.amazonaws.com').text.strip()
-print(f"IP Address: {ipAddr}")
-print(f"Public IP: {publicIp}")
+    downloadSpeed = speedTest.download()
+    downloadMbps = round(downloadSpeed / 1000000, 2)
 
-# ---------- speed test --------------------------------
-speedTest = speedtest.Speedtest()
+    uploadSpeed = speedTest.upload()
+    uploadMbps = round(uploadSpeed / 1000000, 2)
 
-downloadSpeed = speedTest.download()
-downloadMbps = round(downloadSpeed / 1000000, 2)
+    pingTime = round(speedTest.results.ping, 2)
 
-uploadSpeed = speedTest.upload()
-uploadMbps = round(uploadSpeed / 1000000, 2)
+    print(f"Download Speed: {downloadMbps} Mbps" )
+    print(f"Upload Speed: {uploadMbps} Mbps" )
+    print(f"Ping: {pingTime} ms" )
 
-pingTime = round(speedTest.results.ping, 2)
+    # ---------- data entry --------------------------------
+    data = req.getData("http://localhost:3002/api/speeds/read")
 
-print(f"Download Speed: {downloadMbps} Mbps" )
-print(f"Upload Speed: {uploadMbps} Mbps" )
-print(f"Ping: {pingTime} ms" )
+    # Check if the public IP is in the data
+    ip_in_data = any(entry for entry in data if entry['Ip'] == publicIp)
+    
+    if ip_in_data:
+        matching_entry = next((entry for entry in data if entry['Ip'] == publicIp), None)
+        print("Editing existing data for IP:", publicIp)
+        item_id = matching_entry['_id']
+        item_name = matching_entry['name']
+        item_download = matching_entry['download']
+        item_upload = matching_entry['upload']   
+        item_upload.append(f'{uploadMbps}')
+        item_download.append(f'{downloadMbps}')
+        req.updateData(publicIp, item_name, item_download, item_upload, pingTime, date_and_time, item_id)
 
-# ---------- data entry --------------------------------
-data = req.getData("http://localhost:3002/api/speeds/read")
+    else:
+        print("Creating new data for IP:", publicIp)
+        name = ""
+        req.postData(publicIp, name, downloadMbps, uploadMbps, pingTime, date_and_time)
 
-# Check if the public IP is in the data
-ip_in_data = any(entry for entry in data if entry['Ip'] == publicIp)
-
-if ip_in_data:
-    matching_entry = next((entry for entry in data if entry['Ip'] == publicIp), None)
-    print("Editing existing data for IP:", publicIp)
-    item_id = matching_entry['_id']
-    item_name = matching_entry['name']
-    req.updateData(publicIp, item_name, downloadMbps, uploadMbps, pingTime, date_and_time, item_id)
-
-else:
-    print("Creating new data for IP:", publicIp)
-    name = ""
-    req.postData(publicIp, name, downloadMbps, uploadMbps, pingTime, date_and_time)
+# ---------- code timer --------------------------------
+while True:
+    start_time = time.time()
+    speedTest()
+    end_time = time.time()
+    execution_time = end_time - start_time
+    time.sleep(max(10 - execution_time, 0)) 
