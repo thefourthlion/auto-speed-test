6e391e715a49780ed001d12bd75dc039b92c297c
thefourthlion <the00110100lion@gmail.com>
2024-01-23 11:45:26 -0800

    change api url

--- services/apiRequests.py
+++ services/apiRequests.py
@@ -1,15 +1,17 @@
 import requests
 
-api_url = "http://localhost:3024/api/speeds/read"
+api_url = "http://api.speeds.everettdeleon.com/api/speeds/read"
 
 def postData(Ip, name, download, upload, ping, timestamp):
-    api_url = "http://localhost:3024/api/speeds/create"
+    api_url = "http://api.speeds.everettdeleon.com/api/speeds/create"
     data = {"Ip": Ip, "name": name, "download": download, "upload": upload, "ping": ping, "timestamp":timestamp}
+    print(data)
     response = requests.post(api_url, json=data)
     
 def updateData(Ip, name, download, upload, ping, timestamp, id):
-    api_url = f"http://localhost:3024/api/speeds/update/{id}"
+    api_url = f"http://api.speeds.everettdeleon.com/api/speeds/update/{id}"
     data = {"Ip": Ip, "name": name, "download": download, "upload": upload, "ping": ping, "timestamp":timestamp}
+    print(data)
     response = requests.post(api_url, json=data)
 
 # postData("192.168.1.1", "everett_pc", "850", "20", "22", formatted_date_and_time)
