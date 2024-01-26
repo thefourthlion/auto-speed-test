cae254c783e7a68226a5a5e46232a89a58bac385
thefourthlion <the00110100lion@gmail.com>
2024-01-26 09:27:44 -0800

    change timer to 30 min

--- services/main.py
+++ services/main.py
@@ -75,4 +75,4 @@ while True:
     speedTest()
     end_time = time.time()
     execution_time = end_time - start_time
-    time.sleep(max(5 - execution_time, 0)) 
+    time.sleep(max(1800 - execution_time, 0)) 
