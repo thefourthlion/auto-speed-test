98831dd30aa1a0f540f2c2dfd0cada34f2c009a2
thefourthlion <the00110100lion@gmail.com>
2024-02-01 11:55:45 -0800

    add groups db

---
+++ server/models/Groups.js
@@ -0,0 +1,1 @@
+const mongoose = require("mongoose"); const GroupsSchema = new mongoose.Schema( {name: { type: String, required: [true, "Please provide name"],  }, }, { timestamps: true } ); const Groups = mongoose.model("Groups", GroupsSchema); module.exports = Groups;
