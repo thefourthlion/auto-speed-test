c25c97cabf212850f4ec350c034ef38b4f34e27b
thefourthlion <the00110100lion@gmail.com>
2024-02-15 10:34:57 -0800

    take name tag off sh script

--- services/nodeMailer/run-docker.sh
+++ services/nodeMailer/run-docker.sh
@@ -2,4 +2,4 @@
 
 docker build -t speedtest-services-email .
 
-docker run -dp --name ed-speedtest-services-email 3004:3004 speedtest-services-email 
+docker run -dp 3004:3004 speedtest-services-email 
