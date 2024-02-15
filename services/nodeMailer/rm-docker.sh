7f07081be699e4eae8aad7564ff3e98a18ba9deb
thefourthlion <the00110100lion@gmail.com>
2024-02-15 10:42:43 -0800

    combine bash files

--- services/nodeMailer/rm-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# stop and delete container
-docker stop speedtest-services-email
-docker rm speedtest-services-email
-
-# delete image
-docker image rm speedtest-services-email
--- services/nodeMailer/run-docker.sh
+++ services/nodeMailer/run-docker.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# stop and delete container
+docker stop ed-speedtest-services-email
+docker rm ed-speedtest-services-email
+
+# delete image
+docker image rm speedtest-services-email
+
 # pull latest image and run as container
 docker build -t speedtest-services-email .
 
