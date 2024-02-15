53a83fce1660290bb66ce48a0d78f87b9a9d48a3
thefourthlion <the00110100lion@gmail.com>
2024-02-15 10:39:36 -0800

    add delete function and name

--- services/nodeMailer/run-docker.sh
+++ services/nodeMailer/run-docker.sh
@@ -10,4 +10,4 @@ docker image rm speedtest-services-email
 # pull latest image and run as container
 docker build -t speedtest-services-email .
 
-docker run -dp 3004:3004 speedtest-services-email 
+docker run -dp 3004:3004 --name ed-speedtest-services-email speedtest-services-email 
