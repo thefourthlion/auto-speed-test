fae7fd7eab2d0884c340dc7dda1be7ca08aba6ff
everettthefourth <the00110100lion@gmail.coma>
2024-04-10 06:13:14 -0700

    change ports

--- server/run-docker.sh
+++ server/run-docker.sh
@@ -10,4 +10,4 @@ docker image rm speedtest-server
 # pull latest image and run as container
 docker build -t speedtest-server .
 
-docker run -dp 3025:3025 --name ed-speedtest-server speedtest-server 
+docker run -dp 4001:4001 --name ed-speedtest-server speedtest-server 
